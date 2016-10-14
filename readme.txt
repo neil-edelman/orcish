381ae6e9620cf139b8ac5f34be99f9b513d3e283
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2016-10-14 19:14:26 -0400

    Merged Sort into Common.

---
+++ readme.txt
@@ -0,0 +1,15 @@
+2014 Neil Edelman.
+neil dot edelman each mail dot mcgill dot ca
+
+Version 1.1.
+
+Orcish words are gathered off the Internet, SMAUG1.8, made up myself,
+etc. They originate or are inspired by JRR Tolkien's Orcish; this
+file has had many incarnations and was ported from Java. Random
+words are super-useful in testing and character generation.  {@link
+http://en.wikipedia.org/wiki/Languages_constructed_by_J._R._R._Tolkien}.
+
+ANSI C89; provides one function, @code{void Orcish(char *const name,
+const size_t name_size)}, which takes @code{name}, a string, and
+replaces it, to a maximum of @code{name_size} characters, with a
+@code{rand()} Orcish name.
--- src/Orcish.c
+++ src/Orcish.c
@@ -1,10 +1,16 @@
-/** 2014 Neil Edelman; Orcish words are gathered off the Internet, SMAUG1.8,
- made up myself, etc. They originate or are inspired by JRR Tolkien's Orcish;
- this file has had many incarnations and was ported from Java. Random words are
- super-useful in testing and character generation.
- {@link http://en.wikipedia.org/wiki/Languages_constructed_by_J._R._R._Tolkien}.
+/** 2014 Neil Edelman.
+ neil dot edelman each mail dot mcgill dot ca
  <p>
- ANSI C89?
+ Orcish words are gathered off the Internet, SMAUG1.8, made up myself,
+ etc. They originate or are inspired by JRR Tolkien's Orcish; this
+ file has had many incarnations and was ported from Java. Random
+ words are super-useful in testing and character generation.  {@link
+ http://en.wikipedia.org/wiki/Languages_constructed_by_J._R._R._Tolkien}.
+ <p>
+ ANSI C89; provides one function, @code{void Orcish(char *const name,
+ const size_t name_size)}, which takes @code{name}, a string, and
+ replaces it, to a maximum of @code{name_size} characters, with a
+ @code{rand()} Orcish name.
 
  @author	Neil
  @version	1.1, 2016-09
